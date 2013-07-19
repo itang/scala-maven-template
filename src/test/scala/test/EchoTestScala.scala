ef4466192031fa5ed699495db1630736cc2cc0b6
itang <live.tang@gmail.com>
2013-07-19 09:38:16 +0800

    u

--- src/test/scala/test/EchoTestScala.scala
+++ src/test/scala/test/EchoTestScala.scala
@@ -1,8 +1,11 @@
 package test
 
 import org.scalatest.junit.AssertionsForJUnit
+import org.scalatest.junit.JUnitRunner
+import org.junit.runner.RunWith
 import org.junit.Test
 
+@RunWith(classOf[JUnitRunner])
 class EchoTestScala extends AssertionsForJUnit {
 
   @Test def test() {
